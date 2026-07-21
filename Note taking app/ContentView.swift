ee6afa697cf28cf968d837d48d1ac6b93f4499ab
fluffbyte57 <beastminerz2012@gmail.com>
2026-07-21 18:43:32 +0200

    Initial Commit

---
+++ Note taking app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Note taking app
+//
+//  Created by Yacov Vladimirovich on 7/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Note taking app/Note_taking_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_taking_appApp.swift
+//  Note taking app
+//
+//  Created by Yacov Vladimirovich on 7/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Note_taking_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
